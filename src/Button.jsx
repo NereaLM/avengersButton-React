cba7ad21bc1f93d62f4a6a6c50f77d0b351ae386
NereaLosanezmrn <nerea.moreno@bootcamp-upgrade.com>
2022-02-19 15:17:53 +0100

    avengers2

--- src/Button.jsx
+++ src/Button.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Button(props) {
     console.log(props)
   return (
-    <button>{props.children}</button>
+    <button className='button'>{props.children}</button>
   )
 }
+ 
--- src/Saludo.jsx
+++ src/Saludo.jsx
@@ -4,9 +4,10 @@ import Button from './Button'
 export default function Saludo({ vengador, nombre}) {
   return (
       <>
-      <h1>Hola Soy {vengador} y mi nombre real es {nombre}</h1>
-      <Button>ver más info de {vengador}</Button>
+      <h1 className='h1'>Hola Soy {vengador} y mi nombre real es {nombre}</h1>
+      <Button className='button'>ver más info de {vengador}</Button>
       </>
 )
 
 }
+   
